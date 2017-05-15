e101d73eee51f19b6907b0386e2f8a76e4a771a5
kaamraan kamaal <kaamraank@outlook.com>
2017-05-15 16:32:49 +1000

    serial works!!!!

--- ENGG4810-Multimeter/SerialFacade.cs
+++ ENGG4810-Multimeter/SerialFacade.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ENGG4810_Multimeter
@@ -13,6 +14,7 @@ namespace ENGG4810_Multimeter
     {
         private static SerialFacade INSTANCE;
         private SerialPort port;
+        private bool isConnected = false;
 
         //public bool NewDataReceived { get; set; }
         //public int NewDataCount { get; set; }
@@ -51,9 +53,33 @@ namespace ENGG4810_Multimeter
                 port.Open();
                 if (port.ReadChar() != 0)
                 {
+                    isConnected = true;
                     this.port = port;
-                    this.port.DataReceived += Port_DataReceived;
+                    //this.port.DataReceived += Port_DataReceived;
                     Debug.Print($"Port connected successfully on {portName}");
+                    Task.Factory.StartNew(() =>
+                    {
+                        while(this.isConnected && port.IsOpen)
+                        {
+                            int counter = 0;
+                            byte checker = 0;
+                            byte[] buffer = new byte[4];
+
+                            try
+                            {
+                                checker = (byte)port.ReadByte();
+                                while (checker != 0)
+                                {
+                                    buffer[counter++] = checker;
+                                    checker = (byte)port.ReadByte();
+                                }
+                                counter = 0;
+                            }
+                            catch (Exception){}
+                            Debug.Write("buffer: " + BitConverter.ToString(buffer));
+                            Debug.WriteLine(" float " + BitConverter.ToSingle(buffer, 0));
+                        }
+                    });
                     return true;
                 }
                 port.Close();
@@ -86,15 +112,13 @@ namespace ENGG4810_Multimeter
             //port.ReadByte();
             byte[] buffer = new byte[4];
             port.Read(buffer, 0, 4);
-            //foreach (var data in buffer)
-            //{
-            uint value = BitConverter.ToUInt32(buffer, 0);
-            double calculatedValue = (value / 2525154.0) * 2.1;
-            calculatedValue = Math.Round(calculatedValue, 3);
-            Debug.WriteLine($"{value} read");
+            float f = BitConverter.ToSingle(buffer, 0);
+            
+            Debug.WriteLine("buffer " + BitConverter.ToString(buffer));
+            Debug.WriteLine($"{f} read");
             //Debug.WriteLine(buffer[0] + " read");
             //}
-            IncomingData.Add(calculatedValue.ToString());
+            //IncomingData.Add(calculatedValue.ToString());
             //Debug.Print($"{IncomingData[IncomingData.Count - 1]} received and added to graph\n");
 
             byte[] ack = new byte[1];
